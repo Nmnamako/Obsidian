c20ab5495636f1302ccdf2314dccea025c1b5434
Nmnamako <115051904+Nmnamako@users.noreply.github.com>
2023-04-28 13:27:23 +0900

    Create coder11.java

---
+++ Java/coder11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+  int n = scan.nextInt();
+  for(int i = 1; i <= n; i++) {
+    int count = 0;
+    for (int j = 1; j < i; j++) {
+      if (i % j == 0) {
+        count++;
+      }
+      if (count == 2) break;
+    }
+      if (count == 1) {
+        System.out.print(i + " ");
+      }
+    }
+  }
+}
